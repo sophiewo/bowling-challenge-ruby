be94da221da82aa0e23448a8ff520817ac6b7dfa
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-12-09 16:20:22 +0000

    refactors

--- lib/frame.rb
+++ lib/frame.rb
@@ -7,15 +7,13 @@ class Frame
   end
 
   def roll_1(score)
-    @frame_results[:roll_1] = score.to_i
+    @frame_results[:roll_1] = score
+    @frame_results[:total] = score
   end
 
    def roll_2(score)
-    @frame_results[:roll_2] = score.to_i
+    @frame_results[:roll_2] = score
+    @frame_results[:total] += score
   end
 
-  def frame_total
-    @frame_results[:total] = @frame_results[:roll_1] + @frame_results[:roll_2] 
-    p @frame_results
-  end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -5,15 +5,18 @@ describe Frame do
   expect(Frame).to respond_to(:new)
   end
 
-  it 'adds roll one to frame' do
-    expect(subject.roll_1(5)).to eq 5
+  it 'checks roll score' do
+      expect(subject.roll_1(5)).to eq subject.frame_results[:total]
+  end
+
+
+  it 'checks integer' do
+    expect(subject.roll_1(5)).to be_kind_of(Integer)
   end
 
   it 'sums roll_1 and roll_2' do
     subject.roll_1(4)
     subject.roll_2(5)
-    subject.frame_total
     expect(subject.frame_results[:total]).to eq 9
-    # expect(subject.frame_results.has_key?)
   end
  end
