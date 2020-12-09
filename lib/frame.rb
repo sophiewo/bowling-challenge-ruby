abe97ed1e151d277ec198e2d13c52d3fe1a9a812
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-12-09 12:40:44 +0000

    First commit, Frame first test passing

---
+++ lib/frame.rb
@@ -0,0 +1,11 @@
+class Frame
+
+  def initialize
+    @frame_number = frame_number 
+  end
+
+  def frame_number
+    @frame_number = 1
+  end
+
+end
---
+++ spec/frame_spec.rb
@@ -0,0 +1,7 @@
+require 'frame'
+
+describe Frame do 
+  it 'starts with a frame number 1' do
+  expect(subject.frame_number).to eq 1
+  end
+end
