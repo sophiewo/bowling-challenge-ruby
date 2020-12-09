f76465957b316f7d33def98e2c78547f8c41cbb6
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-12-09 15:25:32 +0000

    Basic frame methods

--- lib/frame.rb
+++ lib/frame.rb
@@ -2,10 +2,24 @@ class Frame
 
   def initialize
     @frame_number = frame_number 
+    @round = {}
   end
+
 
   def frame_number
     @frame_number = 1
   end
 
+  def roll_1(score)
+    @round[:roll_1] = score.to_i
+  end
+
+   def roll_2(score)
+    @round[:roll_2] = score.to_i
+  end
+
+  def score
+    result = @round[:roll_1] + @round[:roll_2] 
+    p result
+  end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -2,6 +2,16 @@ require 'frame'
 
 describe Frame do 
   it 'starts with a frame number 1' do
-  expect(subject.frame_number).to eq 1
+  expect(Frame.new.frame_number).to eq 1
   end
-end
+
+  it 'adds roll one to frame' do
+    expect(subject.roll_1(5)).to eq 5
+  end
+
+  it 'sums roll_1 and roll_2' do
+    subject.roll_1(4)
+    subject.roll_2(5)
+    expect(subject.score).to eq 9
+  end
+ end
